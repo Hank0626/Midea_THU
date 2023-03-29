0f681c3ac9a9ef959f93ca7908ecd6425236222b
hank0626 <hank010626@163.com>
2023-03-29 16:58:19 +0800

    update dataload and gp

--- src/dataload.py
+++ src/dataload.py
@@ -1,6 +1,20 @@
 import os
 import os.path as osp
 import numpy as np
+import time
+
+
+def data_statistics(f):
+    def wrapper(*args, **kwargs):
+        train, test = f(*args, **kwargs)
+        print(f"<========= Loading {kwargs['name']} =========>")
+        print(f"train shape: {train.shape=}")
+        print(f"test shape: {test.shape=}")
+        print(f"<========= Data Loaded =========>")
+        time.sleep(3)
+        return train, test
+
+    return wrapper
 
 
 class MideaData(object):
@@ -11,8 +25,12 @@ class MideaData(object):
             for name in os.listdir(self.data_path)
         }
 
+    @data_statistics
     def get_data(self, name: str = "data_chu_1a", test_num: int = 100):
+        data = self.data[name]
+        data[:, 0] /= 1e6
         n = self.data[name].shape[0]
         test_idx = np.linspace(0, n - 1, num=test_num, endpoint=True).astype(int)
         train_idx = [i for i in range(n) if i not in test_idx]
-        return self.data[name][train_idx], self.data[name][test_idx]
+
+        return data[train_idx], data[test_idx]
